0f1590b416b58132a10f5ed404bfe31095c3a1a0
Laurie Fay <lauriethefish@outlook.com>
2021-01-27 07:46:15 +0000

    Update to Beat Saber 1.13.2.

--- Android.mk
+++ Android.mk
@@ -20,23 +20,26 @@ rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
 # Build the modloader shared library
 include $(CLEAR_VARS)
+LOCAL_MODULE := 
+include $(CLEAR_VARS)
+# Creating prebuilt for dependency: beatsaber-hook - version: 1.0.12
+include $(CLEAR_VARS)
+LOCAL_MODULE := beatsaber-hook_1_0_12
+LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_0_12.so
+LOCAL_CPP_FEATURES += exceptions
+include $(PREBUILT_SHARED_LIBRARY)
+# Creating prebuilt for dependency: codegen - version: 0.6.2
+include $(CLEAR_VARS)
+LOCAL_MODULE := codegen_0_6_2
+LOCAL_EXPORT_C_INCLUDES := extern/codegen
+LOCAL_SRC_FILES := extern/libcodegen_0_6_2.so
+include $(PREBUILT_SHARED_LIBRARY)
+# Creating prebuilt for dependency: modloader - version: 1.0.4
+include $(CLEAR_VARS)
 LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
 LOCAL_SRC_FILES := extern/libmodloader.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-# Build the beatsaber-hook shared library, SPECIFICALLY VERSIONED!
-include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_0_9
-LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_0_9.so
-LOCAL_CPP_FEATURES += exceptions
-include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: codegen - version: 0.4.0
-include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_5_3
-LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_5_3.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -45,8 +48,8 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_0_9
-LOCAL_SHARED_LIBRARIES += codegen_0_5_3
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_0_12
+LOCAL_SHARED_LIBRARIES += codegen_0_6_2
 LOCAL_LDLIBS += -llog
 LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -DID='"no-health-and-safety"' -DVERSION='"0.1.1"' -I'./shared' -I'./extern' -isystem'extern/codegen/include'
 LOCAL_CPPFLAGS += -std=c++2a
--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -2,5 +2,5 @@
 & $PSScriptRoot/build.ps1
 
 if ($?) {
-    Compress-Archive -Path "./libs/arm64-v8a/libno-health-and-safety.so", "./libs/arm64-v8a/libbeatsaber-hook_1_0_9.so", "./libs/arm64-v8a/libcodegen_0_5_3.so", "./bmbfmod.json" -DestinationPath "./no-health-and-safety_v0.1.1.zip" -Update
+    Compress-Archive -Path "./libs/arm64-v8a/libno-health-and-safety.so", "./bmbfmod.json" -DestinationPath "./no-health-and-safety_v0.1.1.zip" -Update
 }
